0266495e71f9bb33a57340ff8f130d2ff19e1a0c
mikok <84585119+miko1034@users.noreply.github.com>
2025-03-13 22:59:22 +0000

    changes and stuff wokring

--- flask-server/server.py
+++ flask-server/server.py
@@ -11,15 +11,15 @@ def send_chat():
 
 @app.route("/get_chats")
 def get_chats():
-    with open("chats.txt", "r") as c:
+    #opens chats file
+    with open("flask-server/chats.txt", "r") as c:
         contents = list(c.readlines())
-        all_chats = {"chats":[]}
+        all_chats = {"chats": []}
         for el in contents: # sender | message
             sender, message = el.split(" | ")
-            all_chats["chats"].append({sender:message})
+            all_chats["chats"].append({"sender":sender, "message":message.strip("\n")})
     print(all_chats)
     return jsonify(all_chats)
 
 if __name__ == "__main__":
-    print(os.chdir("flask-server"))
     app.run(debug=True)
